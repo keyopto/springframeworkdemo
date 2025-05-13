9bc8cc58fb55522e8f6acd3c2351690bc5e4481e
keyopto <keyopto@gmail.com>
2025-05-13 16:11:21 +0200

    feat: create author and book classes

---
+++ src/main/java/com/example/demo/Book.java
@@ -0,0 +1,65 @@
+package com.example.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import java.util.Set;
+
+@Entity
+public class Book {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  private String name;
+  private Long nbPages;
+
+  @ManyToMany(mappedBy = "books")
+  private Set<Author> authors;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Long getNbPages() {
+    return nbPages;
+  }
+
+  public void setNbPages(Long nbPages) {
+    this.nbPages = nbPages;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null) return false;
+    if (getClass() != obj.getClass()) return false;
+    Book other = (Book) obj;
+    if (id == null) {
+      if (other.id != null) return false;
+    } else if (!id.equals(other.id)) return false;
+    return true;
+  }
+}
